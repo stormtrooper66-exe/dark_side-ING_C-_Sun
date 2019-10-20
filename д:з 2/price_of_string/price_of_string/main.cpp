7986b8ed8e457eb947cc97793d2085f02a2ffb34
George <george.developer@mail.ru>
2019-10-20 20:59:22 +0300

    д/з_2.0 (21.10.19)

--- д:з 2/price_of_string/price_of_string/main.cpp
+++ д:з 2/price_of_string/price_of_string/main.cpp
@@ -1,19 +1,43 @@
 #include <cstring>
 #include <iostream>
 #include <string>
-#include <clocale>
+#include <deque>
 using namespace std;
-
-int main()
+int utf8_to_unicode(deque<int> &coded)
 {
-    setlocale(LC_CTYPE, "rus");
-    string str;
-    getline(cin,str);
-    int colvo = str.size();
-    double money = (colvo * 0.23f);
-    cout << money << endl ;
+    int charcode = 0;
+    int t = coded.front();
+    coded.pop_front();
+    if (t < 128)
+    {
+        return t;
+    }
+    int high_bit_mask = (1 << 6) -1;
+    int high_bit_shift = 0;
+    int total_bits = 0;
+    const int other_bits = 6;
+    while((t & 0xC0) == 0xC0)
+    {
+        t <<= 1;
+        t &= 0xff;
+        total_bits += 6;
+        high_bit_mask >>= 1;
+        high_bit_shift++;
+        charcode <<= other_bits;
+        charcode |= coded.front() & ((1 << other_bits)-1);
+        coded.pop_front();
+    }
+    charcode |= ((t >> high_bit_shift) & high_bit_mask) << total_bits;
+    return charcode;
+}
+int main ()
+{
+   string str;
+   getline(cin,str);
+   int colvo = str.size();
+   double money = (colvo * 0.23f);
+   cout << money << endl ;
+
     
-    
-    return 0;
 }
 
